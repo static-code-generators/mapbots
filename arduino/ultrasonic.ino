e94ccc18737d2d78d9d18e3aad5037d0efa9a9e6
parthmittal <parth15069@iiitd.ac.in>
2016-02-09 14:21:53 +0530

    Added write to esp module

--- arduino/ultrasonic.ino
+++ arduino/ultrasonic.ino
@@ -6,37 +6,70 @@ int minimumRange = 0; // Minimum range needed
 float pingTime=0;
 float targetDistance=0; // Duration used to calculate distance
 int speedofSound=336; //constant in m/s
+
+#define SSID "[ECE CLUB ROOM]"
+#define PASS "ece@iiitd"
+#define IP "10.0.0.5" // Server IP
+
+boolean writeString (char *buffer, int len);
+boolean connectWiFi();
+
 void setup() {
-  Serial.begin (9600);
-  pinMode(trigPin, OUTPUT); //sonic pulse from trigPin
-  pinMode(echoPin, INPUT);   //detection at Echo Pin
-  pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
+	Serial.begin (9600);
+	pinMode(trigPin, OUTPUT); //sonic pulse from trigPin
+	pinMode(echoPin, INPUT);   //detection at Echo Pin
+	pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
 }
 void loop() {
-  digitalWrite(trigPin, LOW); 
-  delayMicroseconds(2000); 
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(20); 
-  digitalWrite(trigPin, LOW);
-  pingTime = pulseIn(echoPin, HIGH);
-  /* Calculate the distance (in metres) based on the speed of sound. */
-  pingTime=pingTime/1000000.; //calculate time in seconds
-  targetDistance=(speedofSound*pingTime); //twice distance in metres
-  targetDistance=targetDistance/2;
-  if (targetDistance >= maximumRange || targetDistance <= minimumRange)
-  {
-    /* Send a negative number to computer and Turn LED ON 
-    to indicate "out of range" */
-    Serial.println("-1");
-    digitalWrite(LEDPin, HIGH); 
-  }
-  else {
-    /* Send the distance to the computer using Serial protocol, and
-    turn LED OFF to indicate successful reading. */
-    Serial.println(targetDistance);
-    digitalWrite(LEDPin, LOW); 
-  }
-  //Delay 1s before next reading.
-  delay(1000);
+	digitalWrite(trigPin, LOW); 
+	delayMicroseconds(2000); 
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(20); 
+	digitalWrite(trigPin, LOW);
+	pingTime = pulseIn(echoPin, HIGH);
+	/* Calculate the distance (in metres) based on the speed of sound. */
+	pingTime=pingTime/1000000.; //calculate time in seconds
+	targetDistance=(speedofSound*pingTime); //twice distance in metres
+	targetDistance=targetDistance/2;
+	if (targetDistance >= maximumRange || targetDistance <= minimumRange)
+	{
+		/* Send a negative number to computer and Turn LED ON 
+		   to indicate "out of range" */
+		writeString("-1", 2);
+		digitalWrite(LEDPin, HIGH); 
+	}
+	else {
+		/* Send the distance to the computer using Serial protocol, and
+		   turn LED OFF to indicate successful reading. */
+		String targetDistanceString = String(targetDistance);
+		char * targetDistanceCString = targetDistanceString.c_str();
+		writeString(targetDistanceCString, strlen(targetDistanceCString));	
+		digitalWrite(LEDPin, LOW); 
+	}
+	//Delay 1s before next reading.
+	delay(1000);
+}
+boolean connectWiFi() {
+	Serial.println("AT+CWMODE=1");
+	delay(2000);
+	String cmd="AT+CWJAP=\"";
+	cmd+=SSID;
+	cmd+="\",\"";
+	cmd+=PASS;
+	cmd+="\"";
+	sendDebug(cmd);
+	delay(5000);
+	if(Serial.find("OK")) {
+		monitor.println("RECEIVED: OK");
+		return true;
+	} else {
+		monitor.println("RECEIVED: Error");
+		return false;
+	}
 }
 
+boolean writeString (char *buffer, int len) {
+	Serial.print("AT+CIPSEND=0,");
+	Serial.print(len + 1);
+	Serial.println(buffer);
+}
