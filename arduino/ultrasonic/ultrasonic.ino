28484e9e242b7586137ddc40e67f291bf4be9cee
Jai Luthra <me@jailuthra.in>
2016-02-10 15:38:18 +0530

    Some changes

--- arduino/ultrasonic/ultrasonic.ino
+++ arduino/ultrasonic/ultrasonic.ino
@@ -16,9 +16,9 @@ const int speedofSound = 336; //constant in m/s
 
 int id = 0;
 
-boolean writeString (String buffer, int len);
-boolean connectWiFi();
-boolean connectServer();
+int writeString (String buffer, int len);
+int connectWiFi();
+int connectServer();
 void errorLED(int errorCode);
 
 enum errorType
@@ -29,7 +29,7 @@ enum errorType
   badData
 };
 
-SoftwareSerial espSerial(10,11); // RX, TX
+SoftwareSerial espSerial(10, 11); // RX, TX
 
 void setup() {
   Serial.begin(115200);
@@ -43,33 +43,42 @@ void setup() {
   pinMode(echoPin, INPUT);   //detection at Echo Pin
   pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
 
-  while (!connectWiFi())
+  while (!connectWiFi()) {
     errorLED(noWiFi);
-  while (!connectServer())
+  }
+  int temp;
+  while ((temp = connectWiFi()) == 0) {
+    errorLED(noWiFi);
+    Serial.println("connectWiFi: " + String(temp));
+    temp = connectServer();
+  }
+  while ((temp = connectServer()) == 0) {
     errorLED(noServer);
-
+    Serial.println("connectServer: " + String(temp));
+    temp = connectServer();
+  }
 }
 
 void loop() {
   delay(2000);
   /*while (espSerial.available() > 0) {
     Serial.write(espSerial.read());
-  }*/
+    }*/
   espSerial.flush();
-  
-  digitalWrite(trigPin, LOW); 
-  delayMicroseconds(2000); 
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2000);
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(20); 
+  delayMicroseconds(20);
   digitalWrite(trigPin, LOW);
   pingTime = pulseIn(echoPin, HIGH);
   /* Calculate the distance (in metres) based on the speed of sound. */
-  pingTime=pingTime/1000000.; //calculate time in seconds
-  targetDistance=(speedofSound*pingTime); //twice distance in metres
-  targetDistance=targetDistance/2;
+  pingTime = pingTime / 1000000.; //calculate time in seconds
+  targetDistance = (speedofSound * pingTime); //twice distance in metres
+  targetDistance = targetDistance / 2;
   if (targetDistance >= maximumRange || targetDistance <= minimumRange)
   {
-    /* Send a negative number to computer and Turn LED ON 
+    /* Send a negative number to computer and Turn LED ON
        to indicate "out of range" */
     writeString("1 -1", 4);
     errorLED(badData);
@@ -83,43 +92,44 @@ void loop() {
     Serial.println(targetDistanceString);
     if (!writeString(targetDistanceString, targetDistanceString.length()))
       errorLED(noModule);
-//    digitalWrite(LEDPin, LOW);
+    //    digitalWrite(LEDPin, LOW);
   }
   //Delay 1s before next reading.
   delay(1000);
 }
 
 
-boolean connectWiFi() {
+int connectWiFi() {
   espSerial.print("AT+CWMODE=1\r\n");
   Serial.println("AT+CWMODE=1");
   delay(2000);
-  String cmd="AT+CWJAP=\"";
-  cmd+=SSID;
-  cmd+="\",\"";
-  cmd+=PASS;
-  cmd+="\"";
+  String cmd = "AT+CWJAP=\"";
+  cmd += SSID;
+  cmd += "\",\"";
+  cmd += PASS;
+  cmd += "\"";
   espSerial.print(cmd);
   espSerial.print("\r\n");
   Serial.print(cmd);
   espSerial.print("\r\n");
   delay(5000);
-  if(1) {
+  if (int res = findOK()) {
+    Serial.println("FIND OUT ->" + String(res));
     //    monitor.println("RECEIVED: OK");
-    return true;
+    return 1;
   } else {
-    return false;
+    return 0;
   }
 }
 
-boolean moduleWorking() {
+int moduleWorking() {
   Serial.println("AT");
   espSerial.print("AT");
   espSerial.print("\r\n");
-  return espSerial.find("OK");
+  return findOK();
 }
 
-boolean connectServer() {
+int connectServer() {
   Serial.println("AT+CIPMUX=1");
   espSerial.print("AT+CIPMUX=1\r\n");
   String cmd = "AT+CIPSTART=0,\"TCP\"";
@@ -132,19 +142,20 @@ boolean connectServer() {
   espSerial.print(cmd);
   delay(2000);
 
-  if (1) {
-    return true;
+  if (int res = findOK()) {
+    //Serial.println("FIND OUT ->" + String(res));
+    return 1;
   }
   else {
-    return false;
+    return 0;
   }
 }
 
-boolean writeString (String buffer, int len) {
+int writeString (String buffer, int len) {
   Serial.print("AT+CIPSEND=");
   Serial.print(id);
   Serial.print(",");
-  Serial.print(len + 2);
+  Serial.print(len);
   Serial.print("\r\n");
   Serial.print(buffer);
   Serial.print("\r\n");
@@ -159,7 +170,7 @@ boolean writeString (String buffer, int len) {
   delay(5);
 
   espSerial.print(buffer);
-  
+
   return 1;
 }
 
@@ -173,7 +184,7 @@ void errorLED(errorType error) {
       delay(143);
     }
   } else if (error == noWiFi) {
-    digitalWrite(LEDPin, HIGH); 
+    digitalWrite(LEDPin, HIGH);
     delay(1000);
   } else if (error == noServer) {
     int state = HIGH;
@@ -194,3 +205,35 @@ void errorLED(errorType error) {
   }
   digitalWrite(LEDPin, LOW);
 }
+
+int findOK() {
+  char inData[100];
+  char inChar;
+  int index = 0;
+  delay(5);
+  while (espSerial.available() > 0) // Don't read unless
+    // there you know there is data
+  {
+    //Serial.println(espSerial.available());
+    if (index < 99) // One less than the size of the array
+    {
+      inChar = espSerial.read(); // Read a character
+      inData[index] = inChar; // Store it
+      index++; // Increment where to write next
+    } else {
+      break;
+    }
+  }
+
+  inData[index] = '\0'; // Null terminate the string
+  Serial.println(inData);
+  int foundOK = 0;
+  for (index = 0; inData[index + 1]; index++) {
+    if (inData[index] == 'O' && inData[index + 1] == 'K') {
+      foundOK = 1;
+      break;
+    }
+  }
+  return foundOK;
+}
+
