244934255fefa7616f2506e4fe8ab1e5b338c11e
parthmittal <parth15069@iiitd.ac.in>
2016-03-02 12:10:14 +0530

    Modified loop to move in a rectangular spiral out loop

--- bot_stuff/bot_movement/bot_movement.ino
+++ bot_stuff/bot_movement/bot_movement.ino
@@ -1,18 +1,7 @@
-const int AL = 2;
-const int BL = 3;
-const int AR = 4;
-const int BR = 5;
-const int encL = 6;
-const int encR = 7;
-
-void moveLeft();
-void moveRight();
-void moveForward();
-void moveBackward();
-void stahp();
+#include "Constants.h"
+#include "Direction.h"
 
 void setup() {
-  // put your setup code here, to run once:
     pinMode(AL, OUTPUT);
     pinMode(BL, OUTPUT);
     pinMode(AR, OUTPUT);
@@ -24,51 +13,17 @@ void setup() {
     Serial.begin(9600);
 }
 
+const int timeIncrement = 1000;
+int timeInterval = timeIncrement;
+
 void loop() {
-    // put your main code here, to run repeatedly:
-    moveForward();
+    moveLeft();
     delay(1000);
-}
+    stahp();
 
-
-void moveLeft() {
-    // move left wheel anticlock
-    digitalWrite(AL, HIGH);
-    digitalWrite(BL, LOW);
-    // move right wheel clock
-    digitalWrite(AR, LOW);
-    digitalWrite(BR, HIGH);
-}
-
-void moveRight() {
-    // move left wheel clock
-    digitalWrite(AL, LOW);
-    digitalWrite(BL, HIGH);
-    // move right wheel anticlock
-    digitalWrite(AR, HIGH);
-    digitalWrite(BR, LOW);
-}
-
-void moveForward() {
-    // move left and right clock
-    digitalWrite(AL, LOW);
-    digitalWrite(BL, HIGH);
-    digitalWrite(AR, LOW);
-    digitalWrite(BR, HIGH);
-}
-
-void moveBackward() {
-    // move left and right anticlock
-    digitalWrite(AL, HIGH);
-    digitalWrite(BL, LOW);
-    digitalWrite(AR, HIGH);
-    digitalWrite(BR, LOW);
-}
-
-void stahp() {
-    // stop left and right
-    digitalWrite(AL, LOW);
-    digitalWrite(BL, LOW);
-    digitalWrite(AR, LOW);
-    digitalWrite(BR, LOW);
+    moveForward();
+    delay(timeInterval);
+    timeInterval += timeIncrement;
+    stahp();
+    delay(1000);
 }
