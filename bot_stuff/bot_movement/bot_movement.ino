f085d80eaf13cce5ca1408dd74f1a63760c3962c
parthmittal <parth15069@iiitd.ac.in>
2016-03-04 10:03:30 +0530

    added loop movement, with rudimentary polling of encoder for odometry

--- bot_stuff/bot_movement/bot_movement.ino
+++ bot_stuff/bot_movement/bot_movement.ino
@@ -1,12 +1,16 @@
 #include "Constants.h"
 #include "Direction.h"
 
+const double distIncr = 25;
+double currDist = distIncr;
+
+void moveDistance(long double dist);
+
 void setup() {
     pinMode(AL, OUTPUT);
     pinMode(BL, OUTPUT);
     pinMode(AR, OUTPUT);
     pinMode(BR, OUTPUT);
-
     pinMode(encL, INPUT);
     pinMode(encR, INPUT);
 
@@ -14,9 +18,34 @@ void setup() {
 }
 
 void loop() {
-    moveForward();
-    delay(100);
-    Serial.print(digitalRead(encL), BIN);
-    Serial.print("\n");
-    delay(100);
+	moveDistance(currDist);
+	currDist += distIncr;
+
+	moveLeft();
+  delay(900);
+  stahp();
+}
+
+void moveDistance(long double dist) {
+	moveForward();
+	int reqPulses = dist / distPerPulse;
+	int numPulses = 0;
+  Serial.print("target: ");
+  Serial.println(reqPulses);
+	while (numPulses < reqPulses) {
+		int initValue = digitalRead(encL);
+		int currValue;
+
+    Serial.print("HM: ");
+    Serial.println(numPulses);
+
+		while((currValue = digitalRead(encL)) == initValue) {
+			delay(10);
+		}
+		while((currValue = digitalRead(encL)) != initValue) {
+			delay(10);
+		}
+		++numPulses;
+	}
+	stahp();
 }
