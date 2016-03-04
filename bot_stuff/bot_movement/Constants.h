8ef57099b4ec467e81a2154767524c717436784f
parthmittal <parth15069@iiitd.ac.in>
2016-03-04 10:02:19 +0530

    Added dimensions for encoder and wheel

--- bot_stuff/bot_movement/Constants.h
+++ bot_stuff/bot_movement/Constants.h
@@ -9,4 +9,9 @@ const int BR = 5;
 const int encL = 6;
 const int encR = 7;
 
+//For wheel and encoder dimensions
+const long double circ = 157.07963267948965;
+const int numSlits = 30;
+const long double distPerPulse = circ / numSlits;
+
 #endif
