813ea6e243dedefd047ba6b8d6fcc880d73d546f
parthmittal <parth15069@iiitd.ac.in>
2016-03-25 17:57:20 +0530

    added maxima finding to class houghSpace

--- server_stuff/hough.cpp
+++ server_stuff/hough.cpp
@@ -27,12 +27,70 @@ void houghSpace::initShape()
     }
 }
 
+int houghSpace::isMaxima(table_index idx)
+{
+    int indexDiff[3] = {-1, 0, 1};
+    long long bound = boost::math::pow<numDim>(3);
+    table_index neighbour = idx;
+    //iterate over all neighbours
+    //this makes sense because all neighbours have at most numDim indices
+    //different with a diff in range {-1, 0, 1} which has size 3
+    //please email parth15069@iiitd.ac.in if this doesn't make sense
+    for(long long incr = 0; incr < bound; ++incr) {
+        long long temp = incr;
+        int safe = 1;
+        for (int i = 0; i < numDim; ++i) {
+            neighbour[i] = idx[i] + indexDiff[temp % 3]; 
+            if (neighbour[i] >= m_shape[i] || neighbour[i] < 0)
+                safe = 0;
+            temp /= 3;
+        }
+        if (safe && neighbour != idx && m_votingTable(neighbour) >= m_votingTable(idx))
+            return 0;
+    }
+    return 1;
+}
+
 void houghSpace::addVote(std::vector<float> vote)
 {
-    boost::array<table::index, numDim> idx;
+    table_index idx;
     for (int i = 0; i < numDim; ++i) {
         idx[i] = ROUND(vote[i] / m_res[i]);
         assert(idx[i] <= m_maxVal[i]);
     }
     m_votingTable(idx)++;
+}
+
+table::index getIndex(const table& m, const float* requestedElement, const unsigned short int direction)
+{
+    int offset = requestedElement - m.origin();
+    return(offset / m.strides()[direction] % m.shape()[direction] +  m.index_bases()[direction]); 
+}
+
+table_index getIndexArray(const table& m, const float* requestedElement)
+{
+    table_index _idx;
+    for (unsigned int dir = 0; dir < numDim; dir++)
+    {
+        _idx[dir] = getIndex(m, requestedElement, dir);
+    }
+    return _idx;
+}
+
+
+std::vector< std::vector<float> > houghSpace::getMaxima(int threshold)
+{
+    std::vector< std::vector<float> > maxima;
+    std::vector<float> currMaxima;
+    auto p = m_votingTable.data();
+    for (int i = 0; i < m_votingTable.num_elements(); ++i) {
+        table_index idx = getIndexArray(m_votingTable, p);
+        if(isMaxima(idx) && m_votingTable(idx) >= threshold) {
+            for (int i = 0; i < numDim; ++i) {
+                currMaxima[i] = idx[i] * m_res[i];
+            }
+            maxima.push_back(currMaxima);
+        }
+    }
+    return maxima;
 }
--- server_stuff/hough.h
+++ server_stuff/hough.h
@@ -2,11 +2,13 @@
 
 #include <vector>
 #include "boost/multi_array.hpp"
+#include "boost/math/special_functions/pow.hpp"
 #include <cassert>
 
 const int numDim = 2;
 
 typedef boost::multi_array<float, numDim> table;
+typedef boost::array<table::index, numDim> table_index;
 
 #define ROUND(x) (int)(x + 0.5)
 
@@ -14,11 +16,17 @@ class houghSpace
 {
     std::vector<float> m_res;
     std::vector<float> m_maxVal;
-    boost::array<table::index, numDim> m_shape;
+    table_index m_shape;
     table m_votingTable; 
     void initShape();
+    int isMaxima(table_index idx);
 public:
     houghSpace();
     houghSpace(std::vector<float> res, std::vector<float> maxVal);
     void addVote(std::vector<float> vote);
+    std::vector< std::vector<float> > getMaxima(int threshold);
 };
+
+table::index getIndex(const table& m, const float* requestedElement, const unsigned short int direction);
+
+table_index getIndexArray(const table& m, const float* requestedElement);
