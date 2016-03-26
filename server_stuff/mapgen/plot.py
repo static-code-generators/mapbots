a2ada17252ace786d729e00275fd9be73bfd5d94
parthmittal <parth15069@iiitd.ac.in>
2016-03-26 19:17:13 +0530

    plot: deal with lines with rho = 0, major re-write

--- server_stuff/mapgen/plot.py
+++ server_stuff/mapgen/plot.py
@@ -18,12 +18,37 @@ def line_function_creator(rho, theta):
 def main():
     plt.clf()
     plt.cla()
+    lines = []
     for line in sys.stdin:
-        rho, theta = map(float, line.split())
-        line_function = line_function_creator(rho, theta)
-        X = np.arange(0, 10, 0.2);
-        Y = [line_function(i) for i in X]
-        plt.plot(X, Y)
+        lines.append(tuple(map(float, line.split())))
+
+    #put all the lines which have theta = 0 at the end
+    negative_second = lambda tup : -abs(tup[1])
+    lines.sort(key=negative_second)
+
+    #to know the bounds over which we want to plot lines
+    #which are parallel to the Y-axis
+    maxY, minY = 0, 0 
+    at_zeros = 0
+    for line in lines:
+        rho, theta = line
+        try:
+            line_function = line_function_creator(rho, theta)
+            X = np.arange(0, 10, 0.2)
+            Y = [line_function(i) for i in X]
+            maxY = max(maxY, max(Y))
+            minY = min(minY, min(Y))
+            plt.plot(X, Y)
+        except ZeroDivisionError:
+            #tune bounds to fill empty space matplotlib leaves
+            if at_zeros == 0:
+                at_zeros = 1
+                minY -= 5
+                maxY += 5
+            Y = np.arange(minY, maxY, 0.2)
+            X = [rho for _ in Y]
+            plt.plot(X, Y)
+
     plt.savefig('plot_lines.png')
     print("saved output to plot_lines.png")
 
