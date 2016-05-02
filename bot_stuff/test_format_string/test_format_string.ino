6b6666976cf1cb14924340dc2971b8801f1158f5
RadhikaG <radhikaghosal@gmail.com>
2016-05-02 20:01:15 +0530

    botfinal: test_format_string corrected

--- bot_stuff/test_format_string/test_format_string.ino
+++ bot_stuff/test_format_string/test_format_string.ino
@@ -12,16 +12,18 @@ enum direction
     EAST = 3
 };
 
-int rightAngleDelay = 800;
+int rightAngleDelay = 650;
 Servo myservo;
 
 int xPos = 0;
 int yPos = 0;
 direction currDir = NORTH;
 
+int servoStartAngle = 135;
+
 float distMatrix[3][8];
 
-void turnServoAngle(int startAngle, int stopAngle);
+void turnServoAngle();
 void takeMultipleReadings();
 void takeReading(int row);
 void sendReadings();
@@ -41,13 +43,14 @@ void setup()
     pinMode(echoPin, INPUT);
 
     myservo.attach(servoPin);
-    myservo.write(0);
+    myservo.write(servoStartAngle);
 
     Serial.begin(9600);
 }
 
 void loop()
 {
+    myservo.write(servoStartAngle);
     takeMultipleReadings();
     sendReadings();
     turnLeft();
@@ -57,16 +60,15 @@ void loop()
 void takeMultipleReadings()
 {
     int i;
-    int servoStartAngle = 0;
-    int servoStopAngle = 15;
+    
+    int angle = servoStartAngle;
     for(i = 0; i < 3; i++)
     {
-        turnServoAngle(servoStartAngle, servoStopAngle);
+        myservo.write(angle);
         delay(500);
         takeReading(i);
         delay(500);
-        servoStartAngle += 15;
-        servoStopAngle += 15;
+        angle += 15;
     }
 }
 
@@ -77,6 +79,15 @@ void takeReading(int row)
         distMatrix[row][i] = 0.0;
 
     scanAll(distMatrix[row]); // scanAll(float distCm[])
+
+    // debugging statements
+//    Serial.println("takeReading: ");
+//    Serial.print("row ");
+//    Serial.println(row);
+//    for(i = 0; i < numSens; i++)
+//        Serial.println(distMatrix[row][i]);
+//    Serial.println("takeReading done");
+    // debugging done
 
     delay(1000);
 }
@@ -125,23 +136,42 @@ void sendReadings()
     This is wrt the NWSE from the bot orientation at origin.
     */
 
+    // distMatrix[3][8]
+    
     int i, j;
     float sensorTheta = 0;
     float shiftOfDir[4] = {0.0, M_PI / 2, M_PI, (3 * M_PI) / 2};
     float shiftTheta = shiftOfDir[currDir];
+
+    // Debugging again
+//    Serial.println("Entire matrix of readings:");
+//    for(i = 0; i < 3; i++)
+//    {
+//        for(j = 0; j < numSens; j++)
+//        {
+//            Serial.print(distMatrix[i][j]);
+//            Serial.print(" ");    
+//        }
+//        Serial.print("\n");
+//    }
+//    Serial.println("Matrix done");
+    // debugging done
     
-    // outer for-loop iterates as 0, 7, 6, 5... 1 due to orientation of sensors; we want to go counter-clockwise
-    for(i = numSens; i > 0; i--) {
+    // outer for-loop iterates as 1, 0, 7, 6, 5... 2 due to orientation of sensors; we want to go counter-clockwise
+    for(i = numSens+1; i > 1; i--) {
         for(j = 0; j < 3; j++) {
-            Serial.print(42);
-            Serial.print(" ");
-            Serial.print(distMatrix[i % numSens][j]);
-            Serial.print(" ");
+//            Serial.print(i % numSens);
+//            Serial.print(",");
+//            Serial.print(j);
+//            Serial.print(":");
+            Serial.print(distMatrix[j][i % numSens]);
+            Serial.print(",");
             Serial.print(xPos);
-            Serial.print(" ");
+            Serial.print(",");
             Serial.print(yPos);
-            Serial.print(" ");
-            Serial.println(fmod(sensorTheta + shiftTheta, 2 * M_PI));
+            Serial.print(",");
+            Serial.print(fmod(sensorTheta + shiftTheta, 2 * M_PI));
+            Serial.print("\n");
             sensorTheta += 15 * (180.0 / M_PI); 
         }
     }
