db3ea1a8e3f709965e9566c0959f3811ef9b02ae
parthmittal <parth15069@iiitd.ac.in>
2016-05-02 18:26:53 +0530

    bluetooth: hum file band karega

--- server_stuff/network/bt.py
+++ server_stuff/network/bt.py
@@ -3,7 +3,8 @@ import serial
 serial.Serial("/dev/tty.HC-05-DevB", 9600);
 outfile = open("data.csv", "wb+")
 
-while True:
-    line = ser.readline()
-    outfile.writeline(line)
-    print(line)
+with open("data.csv", "wb+") as outfile:
+    while True:
+        line = ser.readline()
+        outfile.writeline(line)
+        print(line)
