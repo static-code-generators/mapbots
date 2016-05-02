75dcbe60b01d115e75bf0588bfaf7d04fc29d107
RadhikaG <radhikaghosal@gmail.com>
2016-05-02 21:42:07 +0530

    botfinal: some changes idk what I'm jai

--- bot_stuff/bot_final/bot_final.ino
+++ bot_stuff/bot_final/bot_final.ino
@@ -10,20 +10,19 @@
 enum direction
 {
     NORTH = 0,
-    EAST = 1,
+    WEST = 1,
     SOUTH = 2,
-    WEST = 3
+    EAST = 3
 };
 
 // all distances are in millimeters, unless otherwise specified
 // so are your mother's boobs
 // constant values
-const float obstacleAvoidDist = 300; // maximum distance of obstacle at which bot should avoid obstacle
-const int rightAngleDelay = 800; // delay in milliseconds required for bot to turn 90 degrees
+const float obstacleAvoidDist = 100; // maximum distance of obstacle at which bot should avoid obstacle
+const int rightAngleDelay = 650; // delay in milliseconds required for bot to turn 90 degrees
 
 // global variables for usage across functions
 direction currDir = NORTH; // currDir is orientation wrt origin, assuming that robot starts in NORTH direction
-direction pendingDir = EAST;
 int obstacleFound = 0; // flag for checking whether obstacle was found in path of bot
 
 /*
@@ -50,6 +49,9 @@ void moveDistance(double dist);
 void turnLeft();
 float findObstacleDist();
 void updatePos(float distanceTravelled);
+void takeMultipleReadings();
+void takeReading(int row);
+void sendReadings();
 
 void setup()
 {
@@ -72,12 +74,15 @@ void loop()
 {
     while (obstacleFound == 1 || straightLineDist > 2048.0) {
         turnLeft();
+        currDir = (direction)(((int)currDir + 1) % 4);
         takeMultipleReadings();
-        obstacleFound = (distMatrix[0][0] < obstacleAvoidDist);
+        sendReadings();
+        obstacleFound = (distMatrix[0][1] != 0) && (distMatrix[0][1] < obstacleAvoidDist);
         straightLineDist = 0;
     }
 
     moveDistance(unitDist);
+    updatePos(unitDist);
     straightLineDist += unitDist;
     takeMultipleReadings();
     sendReadings();
@@ -88,9 +93,9 @@ void updatePos(float distanceTravelled)
 {
     switch (currDir) {
         case NORTH: yPos += distanceTravelled; break;
-        case EAST: xPos += distanceTravelled; break;
-        case SOUTH: yPos -= distanceTravelled; break;
         case WEST: xPos -= distanceTravelled; break;
+        case SOUTH: yPos -= distanceTravelled; break;
+        case EAST: xPos += distanceTravelled; break;
     }
 }
 
@@ -100,45 +105,97 @@ void sendReadings()
     // x-y coords: in cm
     // sensor theta: in degrees
 
-    // Printing x-y coords
-    Serial.print("x: ");
-    Serial.print(xPos);
-    Serial.print(" ");
-    Serial.print("y: ");
-    Serial.println(yPos);
+    // String format:
+    // distance - float
+    // x - float
+    // y - float
+    // theta - float
 
     // Now printing sensor theta along with sensor data
     // all angles in radians now
     // be careful, the code above talks in degrees at times
+    
+    /* 
+    Convention for direction system is as follows:
+         N
+         |
+    W --   -- E
+         |
+         S
+    The directions are *absolute*. North is defined as the direction
+    where the 0th sensor points to at 0 position of the servo at bot origin. 
+
+    Sensors are arranged as:
+         1 
+         |
+    7 --   -- 3 
+         |
+         5 
+    This is wrt 0 position of servo. 
+
+    The 'theta' being sent is:
+              0
+              |
+    M_PI/2 --   -- 3 * M_PI/2
+              |
+             M_PI
+    This is wrt the NWSE from the bot orientation at origin.
+    */
+
+    // distMatrix[3][8]
+    
     int i, j;
     float sensorTheta = 0;
     float shiftOfDir[4] = {0.0, M_PI / 2, M_PI, (3 * M_PI) / 2};
     float shiftTheta = shiftOfDir[currDir];
 
-    for(i = 0; i < numSens; i++) {
+    // Debugging again
+//    Serial.println("Entire matrix of readings:");
+//    for(i = 0; i < 3; i++)
+//    {
+//        for(j = 0; j < numSens; j++)
+//        {
+//            Serial.print(distMatrix[i][j]);
+//            Serial.print(" ");    
+//        }
+//        Serial.print("\n");
+//    }
+//    Serial.println("Matrix done");
+    // debugging done
+    
+    // outer for-loop iterates as 1, 0, 7, 6, 5... 2 due to orientation of sensors; we want to go counter-clockwise
+    for(i = numSens+1; i > 1; i--) {
         for(j = 0; j < 3; j++) {
-            Serial.print("theta: ");
-            Serial.print(sensorTheta + shiftTheta);
-            Serial.print(" ");
-            Serial.println(distMatrix[i][j]);
+//            Serial.print(i % numSens);
+//            Serial.print(",");
+//            Serial.print(j);
+//            Serial.print(":");
+            Serial.print(distMatrix[j][i % numSens] * 10);
+            Serial.print(",");
+            Serial.print(xPos);
+            Serial.print(",");
+            Serial.print(yPos);
+            Serial.print(",");
+            Serial.print(fmod(sensorTheta + shiftTheta, 2 * M_PI));
+            Serial.print("\n");
             sensorTheta += 15 * (180.0 / M_PI); 
         }
     }
-    Serial.println("readings for this batch complete, madafaka");
+//    Serial.println("readings for this batch complete, madafaka");
 }
 
 void moveDistance(double dist) {
     moveForward();
     int reqPulses = dist / distPerPulse;
     int numPulses = 0;
-    Serial.print("target: ");
-    Serial.println(reqPulses);
+    //Serial.print("target: ");
+    //Serial.println(reqPulses);
     while (numPulses < reqPulses) {
         int initValue = digitalRead(encL);
         int currValue;
 
-        Serial.print("HM: ");
-        Serial.println(numPulses);
+        //Serial.print("HM: ");
+        //Serial.println(numPulses);
 
         while((currValue = digitalRead(encL)) == initValue) {
             delay(10);
